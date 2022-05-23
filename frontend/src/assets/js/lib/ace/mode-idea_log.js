51523abf4f5d86db80dd7552f63a1f85bf60d370
konstantin.annikov <konstantin.annikov@jetbrains.com>
2022-05-23 16:03:15 +0700

    Disabled "fold regions between IDE starts" feature to prevent freezes from https://github.com/annikovk/IntelliJ-Log-Analyzer/issues/11

--- frontend/src/assets/js/lib/ace/mode-idea_log.js
+++ frontend/src/assets/js/lib/ace/mode-idea_log.js
@@ -66,83 +66,83 @@ define('ace/mode/idea_log_highlight_rules', [], function (require, exports, modu
 });
 
 //Folds IDE STARTED ... IDE SHUTDOWN sections
-define("ace/mode/folding/idea-style", [], function (require, exports, module) {
-    "use strict";
-
-    let oop = require("../../lib/oop");
-    let Range = require("../../range").Range;
-    let BaseFoldMode = require("./fold_mode").FoldMode;
-
-    let FoldMode = exports.FoldMode = function (commentRegex) {
-        if (commentRegex) {
-            this.foldingStartMarker = new RegExp(
-                this.foldingStartMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.start)
-            );
-            this.foldingStopMarker = new RegExp(
-                this.foldingStopMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.end)
-            );
-        }
-    };
-    oop.inherits(FoldMode, BaseFoldMode);
-
-    (function () {
-        this.foldingStartMarker = /(\{|\[)[^\}\]]*$|^\s*(\/\*)/;
-        this.foldingStopMarker = /^[^\[\{]*(\}|\])|^[\s\*]*(\*\/)/;
-        this.singleLineBlockCommentRe = /^\s*(\/\*).*\*\/\s*$/;
-        this.tripleStarBlockCommentRe = /^\s*(\/\*\*\*).*\*\/\s*$/;
-        this.startRegionRe = /-+ IDE STARTED -+/;
-        this._getFoldWidgetBase = this.getFoldWidget;
-        this.getFoldWidget = function (session, foldStyle, row) {
-            var line = session.getLine(row);
-
-            if (this.singleLineBlockCommentRe.test(line)) {
-                if (!this.startRegionRe.test(line) && !this.tripleStarBlockCommentRe.test(line))
-                    return "";
-            }
-
-            var fw = this._getFoldWidgetBase(session, foldStyle, row);
-
-            if (!fw && this.startRegionRe.test(line) || row === 0)
-                return "start"; // lineCommentRegionStart
-
-            return fw;
-        };
-        this.getFoldWidgetRange = function (session, foldStyle, row, forceMultiline) {
-            var line = session.getLine(row);
-            if (this.startRegionRe.test(line) || row === 0)
-                return this.getCommentRegionBlock(session, line, row);
-        };
-        this.getCommentRegionBlock = function (session, line, row) {
-            let startColumn = line.search(this.startRegionRe);
-            let maxRow = session.getLength();
-            let startRow = row;
-
-            let lineIdeShutdown = /-+ IDE SHUTDOWN -+/;
-            let lineWebserverStopped = /.*web server stopped.*/;
-            let depth = 1;
-            while (++row < maxRow) {
-                line = session.getLine(row);
-                let lineMatchIdeShutdown = lineIdeShutdown.exec(line);
-                let lineMatchIdeStart = this.startRegionRe.test(line);
-                if (lineMatchIdeStart) {
-                    depth--;
-                    row = row - 1
-                    line = session.getLine(row);
-                }
-                if (lineMatchIdeShutdown && lineMatchIdeShutdown[0]) {
-                    depth--;
-                    if (lineWebserverStopped.exec(session.getLine(row + 1))) {
-                        row = row + 1
-                        line = session.getLine(row);
-                    }
-                }
-                if (!depth) break;
-            }
-
-            let endRow = row;
-            if (endRow > startRow && depth===0) {
-                return new Range(startRow, startColumn, endRow, line.length);
-            }
-        };
-    }).call(FoldMode.prototype);
-});
+// define("ace/mode/folding/idea-style", [], function (require, exports, module) {
+//     "use strict";
+//
+//     let oop = require("../../lib/oop");
+//     let Range = require("../../range").Range;
+//     let BaseFoldMode = require("./fold_mode").FoldMode;
+//
+//     let FoldMode = exports.FoldMode = function (commentRegex) {
+//         if (commentRegex) {
+//             this.foldingStartMarker = new RegExp(
+//                 this.foldingStartMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.start)
+//             );
+//             this.foldingStopMarker = new RegExp(
+//                 this.foldingStopMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.end)
+//             );
+//         }
+//     };
+//     oop.inherits(FoldMode, BaseFoldMode);
+//
+//     (function () {
+//         this.foldingStartMarker = /(\{|\[)[^\}\]]*$|^\s*(\/\*)/;
+//         this.foldingStopMarker = /^[^\[\{]*(\}|\])|^[\s\*]*(\*\/)/;
+//         this.singleLineBlockCommentRe = /^\s*(\/\*).*\*\/\s*$/;
+//         this.tripleStarBlockCommentRe = /^\s*(\/\*\*\*).*\*\/\s*$/;
+//         this.startRegionRe = /-+ IDE STARTED -+/;
+//         this._getFoldWidgetBase = this.getFoldWidget;
+//         this.getFoldWidget = function (session, foldStyle, row) {
+//             var line = session.getLine(row);
+//
+//             if (this.singleLineBlockCommentRe.test(line)) {
+//                 if (!this.startRegionRe.test(line) && !this.tripleStarBlockCommentRe.test(line))
+//                     return "";
+//             }
+//
+//             var fw = this._getFoldWidgetBase(session, foldStyle, row);
+//
+//             if (!fw && this.startRegionRe.test(line) || row === 0)
+//                 return "start"; // lineCommentRegionStart
+//
+//             return fw;
+//         };
+//         this.getFoldWidgetRange = function (session, foldStyle, row, forceMultiline) {
+//             var line = session.getLine(row);
+//             if (this.startRegionRe.test(line) || row === 0)
+//                 return this.getCommentRegionBlock(session, line, row);
+//         };
+//         this.getCommentRegionBlock = function (session, line, row) {
+//             let startColumn = line.search(this.startRegionRe);
+//             let maxRow = session.getLength();
+//             let startRow = row;
+//
+//             let lineIdeShutdown = /-+ IDE SHUTDOWN -+/;
+//             let lineWebserverStopped = /.*web server stopped.*/;
+//             let depth = 1;
+//             while (++row < maxRow) {
+//                 line = session.getLine(row);
+//                 let lineMatchIdeShutdown = lineIdeShutdown.exec(line);
+//                 let lineMatchIdeStart = this.startRegionRe.test(line);
+//                 if (lineMatchIdeStart) {
+//                     depth--;
+//                     row = row - 1
+//                     line = session.getLine(row);
+//                 }
+//                 if (lineMatchIdeShutdown && lineMatchIdeShutdown[0]) {
+//                     depth--;
+//                     if (lineWebserverStopped.exec(session.getLine(row + 1))) {
+//                         row = row + 1
+//                         line = session.getLine(row);
+//                     }
+//                 }
+//                 if (!depth) break;
+//             }
+//
+//             let endRow = row;
+//             if (endRow > startRow && depth===0) {
+//                 return new Range(startRow, startColumn, endRow, line.length);
+//             }
+//         };
+//     }).call(FoldMode.prototype);
+// });
